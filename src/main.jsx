d711b2c6bcf1827a4e2fe22069977bc2d2278286
Gkatt-code <2020t00881@stu.cmb.ac.lk>
2024-08-04 17:58:20 +0530

    React router routes added for main pages.

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Homepage from './routes/homepage/Homepage.jsx'
+import DashboardPage from './routes/dashboardPage/DashboardPage.jsx'
+import ChatPage from './routes/chatPage/ChatPage.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <Homepage />
-    )
+    element: <Homepage />
   },
   {
-    path: "about",
-    element: <div>About</div>,
+    path: "dashboard",
+    children: [
+      {
+        path: "dashboard",
+        element: <DashboardPage />
+      },
+      {
+        path: "dashboard/chats/:id",
+        element: <ChatPage />
+      }
+    ]
   },
 ]);
 
