0695d12d89ca8a810d57a430c33e332dbc88a2a6
Gkatt-code <2020t00881@stu.cmb.ac.lk>
2024-08-05 22:47:46 +0530

    Backend was added.

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import ImageKit from "imagekit";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+const imagekit = new ImageKit({
+    urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY
+  });
+
+app.get("/api/upload", (req,res) => {
+    const result = imagekit.getAuthenticationParameters();
+})
+
+app.listen(port, () => {
+    console.log("Server running on port 3000");
+})
