8031f32825e83f871d09677e51c9778546d2a781
Gkatt-code <2020t00881@stu.cmb.ac.lk>
2024-08-05 09:55:15 +0530

    Minor CSS changes. issue fix: scrollbar

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -15,6 +15,11 @@ const ChatList = () => {
             <Link>My chat title</Link>
             <Link>My chat title</Link>
             <Link>My chat title</Link>
+            <Link>My chat title</Link>
+            <Link>My chat title</Link>
+            <Link>My chat title</Link>
+            <Link>My chat title</Link>
+            
         </div>
         <hr />
         <div className="upgrade">
