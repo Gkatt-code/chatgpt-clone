b92d9f2ceebbdddd017da7d78d7bb8081cc99542
Gkatt-code <2020t00881@stu.cmb.ac.lk>
2024-08-05 11:29:35 +0530

    Main Chat Page Completed

--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -2,7 +2,36 @@ import './dashboardPage.css'
 
 const DashboardPage = () => {
   return (
-    <div className='dashboardPage'>DashboardPage</div>
+    <div className='dashboardPage'>
+      <div className="texts">
+        <div className="logo">
+          <img src="/logo.png" />
+          <h1>React AI</h1>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src="/chat.png" />
+            <span>Create a New Chat</span>
+          </div>
+          <div className="option">
+            <img src="/image.png" />
+            <span>Analyze Images</span>
+          </div>
+          <div className="option">
+            <img src="/code.png" />
+            <span>Help Me with My Code</span>
+          </div>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input type="text" placeholder='Ask me anything...' />
+          <button>
+            <img src="/arrow.png" alt="" />
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
